084ad7864e48f79d25318aeb2db522593c42a208
ATWA66 <98480275+ATWA66@users.noreply.github.com>
2022-08-04 13:26:24 +0300

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,9 @@
+﻿//task 7
+int x=7, y=4;
+Console.WriteLine("x + y ="+ (x+y));
+Console.WriteLine("x - y ="+ (x-y));
+Console.WriteLine("x * y ="+ (x*y));
+Console.WriteLine("x / y ="+ (x/y));
+Console.WriteLine("x % y ="+ (x%y));
+Console.WriteLine("--x ="+ (x-1));
+Console.WriteLine("++x ="+ (x+1));
