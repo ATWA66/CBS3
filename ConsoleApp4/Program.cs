808b8b9939875bb32b5b9508d422c644d4daf3a1
ATWA66 <98480275+ATWA66@users.noreply.github.com>
2022-08-04 13:25:15 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,8 @@
+﻿//task 4
+Console.WriteLine("Hello, World!");
+double h = 2.4;
+double R = 4.5;
+double S = 2*Math.PI*R*(R + h);
+double V = Math.PI * Math.Pow(R, 2) * h;
+Console.WriteLine("S="+S);
+Console.WriteLine("V="+V);
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,7 @@
+﻿//task 5
+Console.WriteLine("Hello, World!");
+//byte uberflu?; нельзя использовать знак ? в именах переменных
+short _Identifier;
+ushort \u006fIdentifier; // имя считывается по другому
+//int &myVar;  нельзя использовать знак & в именах переменных
+sbyte myVariab1le;
